30c5e8842eeb0014854648f60b35e85af647726d
Lup Yuen Lee <luppy@appkaki.com>
2024-12-16 18:40:59 +0800

    Clean up

--- my-test-script.sh
+++ my-test-script.sh
@@ -10,17 +10,19 @@ date -u
 set -e  ## Exit when any command fails
 set -x  ## Echo commands
 
+## Get the NuttX Hash (Commit ID)
+nuttx_hash=$(git rev-parse HEAD)
 git status
 TZ=UTC0 \
   git --no-pager \
   log -1 \
   --date='format-local:%Y-%m-%dT%H:%M:%S' \
   --format="%cd | %H | %s"
-nuttx_hash=$(git rev-parse HEAD)
 
 ## Visualise the Git Bisect
 git bisect log
 
+## Randomly simulate OK or Error
 random_0_or_1=$(( $RANDOM % 2 ))
 if (( "$random_0_or_1" == "0" )); then
   set +x ; echo "**** Simulate Error" ; set -x
